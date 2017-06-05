c05d85bcbcb01c14d3cdf88ac9e1ef33de118644
HD152537 <chlwjddn317@naver.com>
2017-06-05 16:55:03 +0900

    Create MelonGenreChart.java

---
+++ JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
@@ -0,0 +1,54 @@
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MelonGenreChart
+{
+    public static void main(String[] args)
+    {
+        Map<String, List<Music>> map = new HashMap<>();
+        List<Music> balladList = new ArrayList<>();
+        List<Music> danceList = new ArrayList<>();
+        
+        balladList.add(new Music("팔레트", "아이유"));
+        danceList.add(new Music("I LUV IT", "PSY"));
+        danceList.add(new Music("맞지?", "언니쓰"));
+
+        map.put("발라드", balladList);
+        map.put("댄스", danceList);
+
+        System.out.println("-- << 멜론 장르별 차트 >> --");
+        printMap(map);
+
+        System.out.println("-- << 댄스 2위 곡 변경 >> --");
+        danceList.set(1, new Music("SIGNAL", "트와이스"));
+        map.put("댄스", danceList);
+        printMap(map);
+
+        System.out.println("-- << 댄스 1위 곡 삭제 >> --");
+        danceList.remove(1);
+        map.put("댄스", danceList);
+        printMap(map);
+
+        System.out.println("-- << 전체 리스트 삭제 >> --");
+        map.clear();
+        printMap(map);
+    }
+
+    public static void printMap(Map<String, List<Music>> map)
+    {
+        for (String key: map.keySet()) 
+        {
+            System.out.println("[" + key + "]");
+            int i = 1;
+            for (Music a: map.get(key)) 
+            {
+                System.out.println(i++ + ". " + a.toString());
+            }
+        }
+        System.out.println();
+    }
+}
